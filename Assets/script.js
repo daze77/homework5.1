ab46543bbbc853df34ee7ca3dac8603ae901052c
daze_77 <daze77@gmail.com>
2021-01-31 15:00:07 -0500

    converted some code to JQuery and added images and readme

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,11 @@
 //this is an event listner that applies to all the buttons as they use the same class we needed a new code so that each button is clickable and will trigger the function
 
-var buttons = document.querySelectorAll(".saveButton")
+var buttons = document.querySelectorAll(".saveBtn")
 buttons.forEach((btn) => {
     btn.addEventListener("click", (event) => {
         saveAgendaLocal();
     });
 })
-
-
 
 var date = Date()
 var currentTime = new Date()
@@ -34,59 +32,59 @@ if(localStorage.currentAgenda){
     console.log("found saved agenda",savedAgenda)
 
     //grab the values that are in savedAgenda that we got from locaStorage and use then to populate the Agenda
-    document.querySelectorAll('input')[0].value = savedAgenda._1
-    document.querySelectorAll('input')[1].value = savedAgenda._2
-    document.querySelectorAll('input')[2].value = savedAgenda._3
-    document.querySelectorAll('input')[3].value = savedAgenda._4
-    document.querySelectorAll('input')[4].value = savedAgenda._5
-    document.querySelectorAll('input')[5].value = savedAgenda._6
-    document.querySelectorAll('input')[6].value = savedAgenda._7
-    document.querySelectorAll('input')[7].value = savedAgenda._8
-    document.querySelectorAll('input')[8].value = savedAgenda._9
-    document.querySelectorAll('input')[9].value = savedAgenda._10
-    document.querySelectorAll('input')[10].value = savedAgenda._11
-    document.querySelectorAll('input')[11].value = savedAgenda._12
-    document.querySelectorAll('input')[12].value = savedAgenda._13
-    document.querySelectorAll('input')[13].value = savedAgenda._14
-    document.querySelectorAll('input')[14].value = savedAgenda._15
-    document.querySelectorAll('input')[15].value = savedAgenda._16
-    document.querySelectorAll('input')[16].value = savedAgenda._17
-    document.querySelectorAll('input')[17].value = savedAgenda._18
-    document.querySelectorAll('input')[18].value = savedAgenda._19
-    document.querySelectorAll('input')[19].value = savedAgenda._20
-    document.querySelectorAll('input')[20].value = savedAgenda._21
-    document.querySelectorAll('input')[21].value = savedAgenda._22
-    document.querySelectorAll('input')[22].value = savedAgenda._23
-    document.querySelectorAll('input')[23].value = savedAgenda._24
+    $('input')[0].value = savedAgenda._1
+    $('input')[1].value = savedAgenda._2
+    $('input')[2].value = savedAgenda._3
+    $('input')[3].value = savedAgenda._4
+    $('input')[4].value = savedAgenda._5
+    $('input')[5].value = savedAgenda._6
+    $('input')[6].value = savedAgenda._7
+    $('input')[7].value = savedAgenda._8
+    $('input')[8].value = savedAgenda._9
+    $('input')[9].value = savedAgenda._10
+    $('input')[10].value = savedAgenda._11
+    $('input')[11].value = savedAgenda._12
+    $('input')[12].value = savedAgenda._13
+    $('input')[13].value = savedAgenda._14
+    $('input')[14].value = savedAgenda._15
+    $('input')[15].value = savedAgenda._16
+    $('input')[16].value = savedAgenda._17
+    $('input')[17].value = savedAgenda._18
+    $('input')[18].value = savedAgenda._19
+    $('input')[19].value = savedAgenda._20
+    $('input')[20].value = savedAgenda._21
+    $('input')[21].value = savedAgenda._22
+    $('input')[22].value = savedAgenda._23
+    $('input')[23].value = savedAgenda._24
 }
 
 //This is the function to trigger the collection of values in the agenda and save it to the loca file.  We are not pushing in this case because it is one day only and we want to ensure we recrieve all values
 function saveAgendaLocal(){
     var currentAgenda = {
-        _1: document.querySelectorAll('input')[0].value,
-        _2: document.querySelectorAll('input')[1].value,
-        _3: document.querySelectorAll('input')[2].value,
-        _4: document.querySelectorAll('input')[3].value,
-        _5: document.querySelectorAll('input')[4].value,
-        _6: document.querySelectorAll('input')[5].value,
-        _7: document.querySelectorAll('input')[6].value,
-        _8: document.querySelectorAll('input')[7].value,
-        _9: document.querySelectorAll('input')[8].value,
-        _10: document.querySelectorAll('input')[9].value,
-        _11: document.querySelectorAll('input')[10].value,
-        _12: document.querySelectorAll('input')[11].value,
-        _13: document.querySelectorAll('input')[12].value,
-        _14: document.querySelectorAll('input')[13].value,
-        _15: document.querySelectorAll('input')[14].value,
-        _16: document.querySelectorAll('input')[15].value,
-        _17: document.querySelectorAll('input')[16].value,
-        _18: document.querySelectorAll('input')[17].value,
-        _19: document.querySelectorAll('input')[18].value,
-        _20: document.querySelectorAll('input')[19].value,
-        _21: document.querySelectorAll('input')[20].value,
-        _22: document.querySelectorAll('input')[21].value,
-        _23: document.querySelectorAll('input')[22].value,
-        _24: document.querySelectorAll('input')[23].value,
+        _1: $('input')[0].value,
+        _2: $('input')[1].value,
+        _3: $('input')[2].value,
+        _4: $('input')[3].value,
+        _5: $('input')[4].value,
+        _6: $('input')[5].value,
+        _7: $('input')[6].value,
+        _8: $('input')[7].value,
+        _9: $('input')[8].value,
+        _10: $('input')[9].value,
+        _11: $('input')[10].value,
+        _12: $('input')[11].value,
+        _13: $('input')[12].value,
+        _14: $('input')[13].value,
+        _15: $('input')[14].value,
+        _16: $('input')[15].value,
+        _17: $('input')[16].value,
+        _18: $('input')[17].value,
+        _19: $('input')[18].value,
+        _20: $('input')[19].value,
+        _21: $('input')[20].value,
+        _22: $('input')[21].value,
+        _23: $('input')[22].value,
+        _24: $('input')[23].value,
 
     }
         console.log("This is the current agenda",currentAgenda)
@@ -98,7 +96,7 @@ saveAgendaLocal()
 
 
 //check the current time and update the scheduler based on scheduler time to current time
-var time = document.querySelectorAll('.form-control')
+var time = $('.form-control')
 for (var i =0; i<time.length; i++){
     time.textContent = i
     console.log(time.textContent)
---
+++ README.md
@@ -0,0 +1,23 @@
+# Word Day Scheduler
+
+
+This application was designed to to showcase the use of local storage by implementing a day planning that can save data to the the local storage and retreive that information upon refresh to display it onto the page. 
+
+The original intent was to use JQuery, which was done is some of the code, but we relied on knowledge of JavaScrip for the most part.
+
+Information can be entered into any of the time blocks and once the save button is pushed, the information is saved to the browsers local storage.  Upon refresh or releading of the page, the last saved information will be populed into the time blocks.  This is the case even if the browers is shut down and restarted. 
+
+In addition, the time blocks are color coded.  Anything past the current time will color code to grey, the current time to red and future time slots in green.  As the hours progress, the color coding of the time slots will update (upon refresh or save) to reflect the current state.
+
+
+## __A link to the git repo housing this application can be found here:__
+https://github.com/daze77/workDayScheduler
+
+## __This is a link to the live webpage housing this application__
+https://daze77.github.io/workDayScheduler/
+
+
+## __Following are some images of the application as a reference:__
+
+![Work Day Scheduler 1 - Console Log](/Assets/images/WDSCL.png)
+![Work Day Scheduler 2 - No console log](/Assets/images/WDS.png)
